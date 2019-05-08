aef1d3e2efb470c1be8b8561baee363c87e8e93d
Bari Jonathan <bari.jonathan@live.fr>
2019-05-08 17:42:50 +0200

    Blur + Retournement des cartes en JS

--- ressources/js/main.js
+++ ressources/js/main.js
@@ -9,17 +9,31 @@ var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal 
 btn.onclick = function() {
-  modal.style.display = "block";
+	modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "none";
+	modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+	if (event.target == modal) {
+		modal.style.display = "none";
+	}
+}
+
+// Retournement des cartes
+var cartes = document.getElementsByClassName('flip-card');
+var r = 0;
+for (var i = 0; i < cartes.length; i++){
+	cartes[i].addEventListener('click', function(){
+		if (r == 0){
+			r = 180;
+		} else if (r == 180) {
+			r = 0;
+		}
+		this.firstChild.nextSibling.style.transform = "rotateX(" + r + "deg)";
+	});
 }
